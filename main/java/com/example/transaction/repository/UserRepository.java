86849a85691089d6c600c40fc669c6a487a93399
Sary <162582566+yoonsary@users.noreply.github.com>
2025-05-26 01:16:28 +0900

    Delete main directory

--- main/java/com/example/transaction/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.transaction.repository;
-
-import com.example.transaction.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-    // 사용자 이름(아이디)으로 사용자 찾기
-    Users findByUsername(String username);
-}
